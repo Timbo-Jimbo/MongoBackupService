40b54acc0166e7d11e7ab301f710e81ea67f7105
timbo-tj <84851611+timbo-tj@users.noreply.github.com>
2024-08-14 12:57:21 +1000

    suppose i should keep a changelog!

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## 0.1.1
+
+- Fixed bug where expired backups were not being deleted properly
+
+## 0.1.0
+
+- Initial launch!
--- src/components/dashboard-header.tsx
+++ src/components/dashboard-header.tsx
@@ -8,7 +8,7 @@ export function DashboardHeader () {
                 <div className="flex items-center space-x-4">
                     <Image src="/logo.svg" alt="MongoDB Logo" width={32} height={32} />
                     <div className="text-xl font-bold hidden sm:inline-flex">Mongo Backup Service</div>
-                    <div className="text-sm text-muted-foreground  hidden sm:inline-flex">v0.1.0</div>
+                    <div className="text-sm text-muted-foreground  hidden sm:inline-flex">v0.1.1</div>
                 </div>
                 <LogoutButton />
             </div>
